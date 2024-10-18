afacb190953be99f94318c30ade699972ead98dd
AlejandroV2204 <91919753+AlejandroV2204@users.noreply.github.com>
2024-10-17 23:39:17 -0500

    Se agregó el navbar a la vista menuCliente

--- client/src/pages/MenuCliente.jsx
+++ client/src/pages/MenuCliente.jsx
@@ -2,11 +2,15 @@ import { useState, useEffect } from "react";
 import { getNegocios } from "../api/auth";
 import { ButtonList } from "../components/ButtonList";
 import { Category } from "../components/Category";
+import Navbar from "../components/Navbar";
 
 export default function MenuCliente() {
   const [categories, setCategories] = useState(["All"]);
   const [lugares, setLugares] = useState([]);
   const [filteredLugares, setFilteredLugares] = useState([]);
+  const navigation = [
+    { name: "Inicio", href: "/menuAfiliado", current: true },
+];
 
   const fetchLugares = async () => {
     try {
@@ -37,9 +41,10 @@ export default function MenuCliente() {
       setFilteredLugares(filtered);
     }
   };
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-700 via-gray-600 to-gray-500">
+    <>
+    <Navbar navigation={navigation} logo="/Image/logoblanco.png" />
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-300 via-gray-600 to-gray-500">
       <div className="max-w-5xl w-full text-white py-10 px-6 rounded-lg shadow-2xl bg-gray-800 bg-opacity-80">
         <div className="text-center mt-10 mb-6">
           <h1 className="text-4xl font-extrabold tracking-widest text-cyan-300 uppercase">
@@ -56,5 +61,7 @@ export default function MenuCliente() {
         <Category lugares={filteredLugares} />
       </div>
     </div>
+    </>
   );
 }
+
