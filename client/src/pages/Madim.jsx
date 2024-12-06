832aed6a7c6f59c95d285bc19df5df4bf04e8834
Mgsk8 <144119715+Mgsk8@users.noreply.github.com>
2024-12-06 02:30:19 -0500

    mejoras 3.0

--- client/src/pages/Madim.jsx
+++ client/src/pages/Madim.jsx
@@ -105,7 +105,12 @@ function Madmin() {
   return (
     <div>
       <Navbar
-        navigation={[{ name: "Inicio", href: "/", current: false }, { name: "Usuarios", href: "/menuAdmin", current: true }]}
+        navigation={[
+          { name: "Inicio", href: "/", current: false },
+          { name: "Usuarios", href: "/menuAdmin", current: true },
+          { name: "Visualizar negocios", href: "/menuAdmin_negocios", current: false },
+          { name: "Registrar negocios", href: "/registerCompany_admin", current: false },
+        ]}
         logo="/image/logoblanco.png"
       />
       <div className="max-w-4xl mx-auto p-4 md:p-8">
